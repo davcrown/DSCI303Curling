f8b61c05d08b7a238df10814b7fbc4e476cddd0f
davcrown <dcurmum@gmail.com>
2026-03-30 14:10:20 -0500

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -26,3 +26,22 @@ results.hist(figsize=(16, 20), bins=50, xlabelsize=8, ylabelsize=8)
 shot_info = results.iloc[:, 7:]
 shot_info.describe()
 
+shot_info['Task'] = shot_info['Task'].to_numpy()
+shot_info['Task'] = shot_info['Task'].replace(-1, np.nan)
+shot_info['Task'] = str(shot_info['Task'])
+shot_info['Handle'] = str(shot_info['Handle'])
+shot_info['Points'] = str(shot_info['Points'])
+shot_info['Points'] = shot_info['Points'].replace(-1, np.nan)
+shot_info['TimeOut'] = shot_info['TimeOut'].fillna(0)
+shot_info['PowerPlay'] = shot_info['PowerPlay'].replace([0,1,2],['None','Left','Right'])
+shot_info.info()
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+shot_info_num = shot_info.select_dtypes(include = 'number')
+
+sns.heatmap(shot_info_num.corr(), cmap='coolwarm')
+plt.show()
+golden_features = shot_info_num.corr()['Result'].sort_values(ascending=False)[1:6].index.tolist()
+golden_features
+
